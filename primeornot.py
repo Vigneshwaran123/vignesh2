9066737c4968868941aba8014bc1febee571b1b5
Vigneshwaran123 <51016246+Vigneshwaran123@users.noreply.github.com>
2019-06-11 12:41:53 +0530

    Update primeornot.py

--- primeornot.py
+++ primeornot.py
@@ -1,12 +1,10 @@
-a = int(input())
-if (a<=1000):
-
-    k = 0
-    for i in range(2, a // 2 + 1):
-        if (a % i == 0):
-            k = k + 1
-    if (k <= 0):
-        print("yes")
-
+a=int(input())
+iscomposite=0
+for i in range(2,a):
+  if(a%i==0):
+    iscomposite=1
+    break
+if(iscomposite==1):
+  print("no")
 else:
-    print('no')
+  print("yes")
