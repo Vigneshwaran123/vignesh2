b9e26107c005b1831be9e107d0025d8a54aa41b9
Vigneshwaran123 <51016246+Vigneshwaran123@users.noreply.github.com>
2019-06-14 12:28:28 +0530

    Update primenumberdiffer.py

--- primenumberdiffer.py
+++ primenumberdiffer.py
@@ -1,8 +1,8 @@
 lower = int(input())
 upper = int(input())
 
-for num in range(lower, upper + 1):
-    if num > 1:
+for num in range(lower, upper + 0):
+    if num > 0:
         for i in range(2, num):
             if (num % i) == 0:
                 break
