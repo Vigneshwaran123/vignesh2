fddeba4839541589933d2475f9442a294060623c
Vigneshwaran123 <51016246+Vigneshwaran123@users.noreply.github.com>
2019-06-14 12:35:29 +0530

    Update primenumberdiffer.py

--- primenumberdiffer.py
+++ primenumberdiffer.py
@@ -1,10 +1,12 @@
-lower = int(input())
-upper = int(input())
+a = int(input())
+b = int(input())
+for i in range(a+1,b):
+  if(i>1):
+    for j in range(2,i):
+      if(i%j==0):
+        break
 
-for num in range(lower, upper + 0):
-    if num > 0:
-        for i in range(2, num):
-            if (num % i) == 0:
-                break
-        else:
-            print(num)
+    else:
+      print(i,end=' ')
+  else:
+    break
